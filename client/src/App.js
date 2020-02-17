40faae26e56f56bd82815f5b909c6d093ce6ec67
gachez <briangach98@gmail.com>
2020-02-17 15:57:43 +0300

    Setup map SDK on client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import ReactMapGL from 'react-map-gl';
+
+import {listLogEntries} from './API'
 
 function App() {
+ 
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: -1.29015,
+    longitude: 36.8082,
+    zoom: 10
+  });
+
+  useEffect(() =>{
+    (async () => {
+      const logEntries = await listLogEntries();
+      console.log(logEntries)
+    })();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactMapGL
+      mapStyle="mapbox://styles/gachez/ck6qey7i71wuf1imfkizw8jfc"
+      {...viewport}
+      onViewportChange={setViewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+    />
   );
 }
 
