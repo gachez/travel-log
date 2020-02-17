bc4ac1cbc06ccc4d16e03917f316022d89882247
gachez <briangach98@gmail.com>
2020-02-17 21:11:00 +0300

    more changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,138 @@
 import React, {useState, useEffect} from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {Marker, Popup} from 'react-map-gl';
+import place from './img/placeholder.svg';
+import addplace from './img/maps-and-flags.png';
+import LogEntryForm from './LogEntryForm'
+import {Navbar} from 'react-bootstrap'
 
-import {listLogEntries} from './API'
+import {listLogEntries} from './API';
 
-function App() {
- 
+const  App = () => {
+  const [logEntries, setLogEntries] = useState([]);
+  const [showPopup, setShowPopup] = useState({});
+  const [addEntryLocation, setAddEntryLocation] = useState(null);
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
     latitude: -1.29015,
     longitude: 36.8082,
-    zoom: 10
+    zoom: 3
   });
 
   useEffect(() =>{
     (async () => {
       const logEntries = await listLogEntries();
-      console.log(logEntries)
+      setLogEntries(logEntries);
     })();
-  }, [])
+  }, []);
+
+  const showAddMarkerPopup = (event) => {
+    const [ longitude, latitude] = event.lngLat;
+    setAddEntryLocation({
+      latitude,
+      longitude,
+    })
+  };
 
   return (
-    <ReactMapGL
+   <div>
+  <Navbar bg="dark">
+    <Navbar.Brand href="#home" style={{color: 'white'}}>Mapit</Navbar.Brand>
+
+  </Navbar>
+
+
+  <ReactMapGL
       mapStyle="mapbox://styles/gachez/ck6qey7i71wuf1imfkizw8jfc"
       {...viewport}
       onViewportChange={setViewport}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-    />
+      onDblClick={showAddMarkerPopup}
+    >
+      {
+        logEntries.map(entry => (
+          <React.Fragment  key={entry._id}>
+          <Marker 
+           
+            latitude={entry.latitude} 
+            longitude={entry.longitude}
+            offsetLeft={-12} 
+            offsetTop={-24}
+            >
+          <img 
+            alt="location" 
+            src={place} 
+            width="24px" 
+            height="24px"
+            onClick = {() => setShowPopup({
+              [entry._id]: true
+            })}
+          />
+        </Marker>
+        {
+          showPopup[entry._id] ? (
+            <Popup
+            className="popup"
+            style={{maxWidth: '300px'}}
+            latitude={entry.latitude}
+            longitude={entry.longitude}
+            closeButton={true}
+            closeOnClick={false}
+            dynamicPosition={true}
+            onClose={() => setShowPopup({})}
+            anchor="top" >
+            <div>
+              <h3>{entry.title}</h3>
+              <img alt="location img" src={entry.image} width="200px" height= "200px"/>
+              <p>{entry.comments}</p>
+              <small>Visited on: {new Date(entry.visitDate).toLocaleDateString()}</small>
+            </div>
+        </Popup> 
+          ): null
+        }
+      </React.Fragment>
+    
+        ))
+      }
+      {
+        addEntryLocation ? (
+          <React.Fragment>
+            <Marker 
+                
+                latitude={addEntryLocation.latitude} 
+                longitude={addEntryLocation.longitude}
+                offsetLeft={-12} 
+                offsetTop={-24}
+                >
+              <img 
+                alt="location" 
+                src={addplace} 
+                width="24px" 
+                height="24px"
+             
+              />
+            </Marker>
+            <Popup
+              style={{maxWidth: '300px'}}
+              latitude={addEntryLocation.latitude}
+              longitude={addEntryLocation.longitude}
+              closeButton={true}
+              closeOnClick={false}
+              dynamicPosition={true}
+              onClose={() => setAddEntryLocation(null)}
+              anchor="top" >
+              <div>
+                <LogEntryForm />
+              </div>
+            </Popup> 
+          </React.Fragment>
+         
+
+        ) : null
+      }
+    </ReactMapGL>  
+   </div>
+  
   );
 }
 
---
+++ client/src/LogEntryForm.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+    Form,
+    Col,
+    Button
+} from 'react-bootstrap';
+import { useForm } from 'react-hook-form'
+
+
+const LogEntryForm = () =>{
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
+    return(
+        <Form onSubmit={handleSubmit(onSubmit)} >
+        <Form.Row>
+            <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label>Title</Form.Label>
+            <Form.Control ref={register} type="text" name="title" placeholder="Enter Title" />
+            </Form.Group>
+        </Form.Row>
+        
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Description</Form.Label>
+            <Form.Control ref={register} as="textarea" name="description" rows="3" />
+        </Form.Group>
+        
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Comment</Form.Label>
+            <Form.Control ref={register} as="textarea" name="comment" rows="3" />
+        </Form.Group>
+
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Image</Form.Label>
+            <Form.Control ref={register} type="text" name="image" placeholder="paste img URL here"/>
+        </Form.Group>
+
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Visited at</Form.Label>
+            <Form.Control ref={register} type="date" name="visitDate"/>
+        </Form.Group>
+        
+        <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label>Rating</Form.Label>
+            <Form.Control ref={register} type="number" name="rating" placeholder="x/10" />
+        </Form.Group>
+        
+        
+        <Button variant="primary" type="submit" onClick = {(e) => {
+            e.preventDefault();
+        }}>
+            Create Entry
+        </Button>
+        </Form>
+    )
+}
+
+export default LogEntryForm;
